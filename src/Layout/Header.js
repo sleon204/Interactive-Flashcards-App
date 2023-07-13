d16595bb57510f1f00762ae0fc1fd02d86c2de0a
sleon204 <129817222+sleon204@users.noreply.github.com>
2023-07-13 08:58:24 -0700

    transfer
    Transfered files from qualified

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark mb-4 ">
+    <header className="jumbotron bg-dark">
       <div className="container text-white">
         <h1 className="display-4">Flashcard-o-matic</h1>
         <p className="lead">Discover The Flashcard Difference.</p>
